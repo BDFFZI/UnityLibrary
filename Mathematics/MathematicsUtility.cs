7ada889b80d14bceb1a28074d9760e46fdd727df
BDFFZI <1330958515@qq.com>
2025-09-26 18:05:15 +0800

    -

--- Mathematics/MathematicsUtility.cs
+++
@@ -1,9 +0,0 @@
-using Unity.Mathematics;
-
-public static class MathematicsUtility
-{
-    public static float ComponentSum(this float3 vector)
-    {
-        return vector.x + vector.y + vector.z;
-    }
-}
